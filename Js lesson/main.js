5c5a6a0a868c9a6babcce1e428666e3f1341f198
Ayxan1234 <nicat.huseynov2007@outlook.com>
2024-10-28 18:22:24 +0400

    Js Lesson

---
+++ Js lesson/main.js
@@ -0,0 +1,136 @@
+// let number = prompt("Reqeminizi daxil edin")
+
+// if(number > 0 ){
+//     console.log("Musbet Ededdir"); 
+// }else if(number < 0){
+//     console.log("Menfi Ededdir");
+// }else{
+//     console.log("Sifirdir");
+// }
+
+
+
+// let istifadeci = prompt ("Istifadeci adini daxil edin")
+
+// switch (istifadeci) {
+//     case "admin":
+//         console.log("Salam Admin, Xos Gelmisiniz.!"); break;
+       
+//          case "qonaq":
+//         console.log("Salam Qonaq, Xos Gelmisiniz.!"); break;
+
+//         case "istifadeci":
+//         console.log("Salam Istifadeci, Xos Gelmisiniz.!"); break;
+ 
+//     default:
+//         break;
+// }
+
+
+
+// let eded = prompt ("Reqeminizi daxil edin")
+
+// if(eded % 2 === 0){
+//     console.log(eded + "Eded Cutdur");
+    
+// }else if (eded % 3 === 0){
+//     console.log(eded+ "Eded Tekdir");
+// }else{
+//     console.log("Bu reqem deyil"); 
+// }
+
+
+
+// let name1 = 5
+// let name2 = "5"
+// console.log(name1 == name2);
+
+// console.log(name1 === name2);
+
+
+
+// let number = prompt("reqeminizi daxil edin")
+
+// if(number > 5 && number < 10){
+//     console.log(number, "10 dan kicik ve 5 den boyukdur.!");
+// }else{
+//     console.log(number, "10 dan kicik ve 5 den boyuk deyildir.!");   
+// }
+
+
+
+
+// let a = prompt ("reqem daxil et");
+// let b = prompt ("ikinci reqemi daxil et");
+
+// if ( a && b ){
+//     console.log("her iki sert dogrudur");   
+// }else {
+//     console.log("her iki sert dogru deyil");
+// }
+
+
+// // Boolean qanunu
+
+// let deyer = true;
+// let tersi = !deyer;
+// console.log(tersi);
+
+
+
+// const comption1 = true;  
+// const comption2 = false; 
+
+// if (comption1 || comption2) {
+//     console.log("Bir sert dogrudur.");
+// } else {
+//     console.log("Her iki sert yanlisdir.");
+// }
+
+
+
+// let a = prompt("Reqeminizi daxil edin")
+// if( a > 0){
+//     var b = "musbet"
+// }else{
+//     var b = "menfi"
+// }
+// if (a %2==0){
+//     var c = "cut"
+// }else{
+//     var c = "tek"
+// }
+// console.log(a+" "+b+" "+c);
+
+
+
+
+// const inUsername = "Istifadeci";
+// const inPassword = "parolugizli2024";
+
+// const inputUsername = prompt("Istifadeci adinizi qeyd edin");
+// const inputPassword = prompt("Parolunuz daxil edin");
+
+// if (inUsername === inputUsername && inPassword === inputPassword) {
+//     console.log("Parol dogrudur");
+// } else {
+//     console.log("Istifadeci adi ve parol sehvdir");
+// }
+
+
+
+
+
+// let a = prompt("")
+
+// switch (true) {
+//     case a < 0:
+//         console.log("Ədəd mənfidir.");
+//         break;
+//     case a == 0:
+//         console.log("Ədəd sıfırdır.");
+//         break;
+//     default:
+//         console.log("Ədəd müsbətdir.");
+// }
+
